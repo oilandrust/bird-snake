9fa1a5d10d2cbd453ed419b5a52ee40a10b50ebd
Olivier Rouiller <o.rouiller@gmail.com>
2022-12-29 13:46:51 +0100

    Removed game-grid from the tree

--- game-grid/derive/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use derive_core::derive_grid_cell;
-use proc_macro::TokenStream;
-
-#[proc_macro_derive(GridCell)]
-pub fn grid_cell(input: TokenStream) -> TokenStream {
-    derive_grid_cell(input.into()).into()
-}
